4a0670948589faec825dddf86a7a7b3e64c2c9ba
Voyda2137 <91951966+Voyda2137@users.noreply.github.com>
2023-07-05 20:46:17 +0200

    xd

--- app.ts
+++ app.ts
@@ -1,12 +1,29 @@
 // @ts-ignore
 import express, { Express, Request, Response } from 'express';
-
+import * as bodyParser from "body-parser";
 const app: Express = express()
 const port = 2137
+const cors = require('cors')
+
+app.use(bodyParser.json())
+
+app.use(cors());
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
-
+app.post('/xd', async (req, res) => {
+    try {
+        const alcohol = {
+            name: req.body.name
+        }
+        console.log('alcohol', alcohol)
+        res.sendStatus(200)
+    }
+    catch (e) {
+        res.status(500).send('Could not add alcohol')
+    }
+})
 app.listen(port, () => {
     console.log(`xdd`);
 });
