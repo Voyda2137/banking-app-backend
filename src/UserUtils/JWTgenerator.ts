bbd69cf4399900c0aa9b0dad184dc91974a5e1e5
Voyda2137 <91951966+Voyda2137@users.noreply.github.com>
2023-07-12 22:41:09 +0200

    xd

--- src/UserUtils/JWTgenerator.ts
+++ src/UserUtils/JWTgenerator.ts
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken';
 import {User} from "../models/UserInterface";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 export const generateToken = (user: User): string => {
-    return jwt.sign({ login: user.login }, 'your-secret-key', { expiresIn: '1h' });
+    return jwt.sign({ login: user.login }, process.env.AUTHORIZER_SECRET, { expiresIn: '1h' });
 }
 
