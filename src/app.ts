92206042cf9846499271aec0b4605a677dc300d1
Voyda2137 <91951966+Voyda2137@users.noreply.github.com>
2023-07-12 21:27:37 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,11 @@ import passport from "passport";
 import {connectToMongo} from "./DatabaseUtils/DatabaseUtils";
 import userRouter from "./Routes/User/UserRouter";
 
-const bodyParser = require('body-parser')
-
 const app: Express = express()
 const port = process.env.PORT
 const cors = require('cors')
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(passport.initialize())
 app.use(cors());
 
