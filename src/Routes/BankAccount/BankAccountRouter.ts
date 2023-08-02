06b232a3872728007c70d50a18719efa31511a8f
Voyda2137 <91951966+Voyda2137@users.noreply.github.com>
2023-08-02 18:49:53 +0200

    Update BankAccountRouter.ts

--- src/Routes/BankAccount/BankAccountRouter.ts
+++ src/Routes/BankAccount/BankAccountRouter.ts
@@ -5,16 +5,22 @@ import passport from "../../UserUtils/Authorizer"
 const bankAccountRouter = Router()
 
 bankAccountRouter.post('/create', passport.authenticate('jwt', { session: false }), (req: Request, res: Response) => {
-    createBankAccount(req.body).then(response => {
-        if(response === null){
-            res.status(500).json({ success: false, message: 'Account creation failure' })
-        }
-        else{
-            addAccountToUser(req.body.userId, response._id.toString()).then(() => {
-                res.status(200).json({success: true, message: 'Successfully created the bank account'})
-            })
-        }
-    })
+    try {
+        createBankAccount(req.body).then(response => {
+            if(response === null){
+                return res.status(500).json({ success: false, message: 'Account creation failure' })
+            }
+            else{
+                addAccountToUser(req.body.userId, response._id.toString()).then(() => {
+                    return res.status(200).json({success: true, message: 'Successfully created the bank account'})
+                })
+            }
+        })
+    }
+    catch (e) {
+        console.log(e)
+        return res.status(500).json({ success: false, message: 'Something went wrong' });
+    }
 })
 bankAccountRouter.get('/accounts', passport.authenticate('jwt', { session: false }), (req: Request, res: Response) => {
     getUserAccounts(req.body).then(response => {
