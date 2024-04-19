bd5ed647917fed472ecb568aa4d90a966f76bdde
Maksym <smolinskimaksym@gmail.com>
2024-04-19 12:46:51 +0200

    resolved

--- src/Routes/Currency/CurrencyRouter.ts
+++ src/Routes/Currency/CurrencyRouter.ts
@@ -9,38 +9,35 @@ import {ApiUrlCurrency} from "../../models/CurrencyInterface";
 const currencyRouter = Router();
 
 
-const API_URL = ({date, hour}: ApiUrlCurrency) => `https://cdn.jsdelivr.net/gh/ismartcoding/currency-api@main/${date}/${hour}.json`
+const API_URL = (date: string, hour: string) => `https://cdn.jsdelivr.net/gh/ismartcoding/currency-api@main/${date}/${hour}.json`;
 
 currencyRouter.get('/', passport.authenticate('jwt', {session: false}), async (req, res, next) => {
     try {
-        const authHeader: string | undefined = req.header('Authorization')
+        const user = await getUserFromJwt(req.header('Authorization'));
+        if (!user) throw new Error('Could not verify user');
 
-        const user = await getUserFromJwt(authHeader)
-        if (!user) {
-            throw new Error('Could not verify user')
-        }
+        const dateToday = moment().format('YYYY-MM-DD');
+        const dateYesterday = moment().subtract(1, 'd').format('YYYY-MM-DD');
+        const hour = moment().subtract(6, 'h').format("HH");
 
-        const currenciesTomorrow = await fetch(API_URL({
-            date: moment().subtract(1, 'd').format('YYYY-MM-DD'),
-            hour: moment().subtract(6, 'h').format("HH")
-        })).then(response => response.json())
+        const currenciesTomorrow = await fetch(API_URL(dateYesterday, hour)).then(response => response.json());
+        const currenciesToday = await fetch(API_URL(dateToday, hour)).then(response => response.json());
 
-        const currenciesToday = await fetch(API_URL({
-            date: moment().format('YYYY-MM-DD'),
-            hour: moment().subtract(6, 'h').format("HH")
-        })).then(response => response.json())
-
-        const changes = calculateExchangeRateChangePercentage(currenciesTomorrow, currenciesToday)
+        const changes = calculateExchangeRateChangePercentage(currenciesTomorrow, currenciesToday);
+        const filteredCurrencies = changes.filter((exchangeRate) => exchangeRate.currency.toLowerCase().includes(String(req.query.q).toLowerCase()));
 
         return res.status(200).json({
             success: true,
             message: 'Successfully retrieved currencies',
-            changes,
-            currenciesToday,
-            currenciesTomorrow
-        })
+            changes: {
+                tsToday: currenciesToday.ts,
+                tsTomorrow: currenciesTomorrow.ts,
+                base: currenciesToday.base,
+                quotes: req.query.q ? filteredCurrencies : changes
+            },
+        });
     } catch (e) {
-        next(e)
+        next(e);
     }
 })
 
